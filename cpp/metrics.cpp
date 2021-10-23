944b7675d41a61fe2f3e642bba7e427026cb8ddc
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2021-10-23 20:52:16 +0300

    Baseline mvp algorithm

--- cpp/metrics.cpp
+++ cpp/metrics.cpp
@@ -1,1 +1,72 @@
 #include "metrics.h"
+
+namespace map_metrics{
+
+    Eigen::MatrixX3d cov(Eigen::MatrixX3d const & M){
+        Eigen::MatrixX3d centered = M.rowwise() - M.colwise().mean();
+        return (centered.adjoint() * centered) / (static_cast<double>(M.rows()) - 1.0);
+    }
+
+    Eigen::MatrixX3d points_idx_to_matrix(std::vector<Eigen::Vector3d> const & points, std::vector<int> const & idx){
+        Eigen::MatrixX3d matrix(idx.size(), 3);
+        Eigen::Index row_idx = 0;
+        for (const auto & i : idx){
+            matrix.row(row_idx++) = points[i];
+        }
+        return matrix;
+    }
+
+    PointCloud aggregate_map(std::vector<PointCloud> const & pcs, std::vector<Eigen::Matrix4d> const & ts){
+        assert (pcs.size() == ts.size());
+
+        std::vector<Eigen::Matrix4d> inv_ts(ts.size());
+        for (size_t i = 0; i < ts.size(); ++i)
+            inv_ts[i] = ts[i] * ts[0].inverse();
+
+        PointCloud pc_map = PointCloud();
+        for (size_t i = 0; i < pcs.size(); ++i){
+            PointCloud tmp = pcs[i];
+            pc_map += tmp.Transform(ts[i]);
+        }
+
+        return pc_map;
+    }
+
+    // https://github.com/isl-org/Open3D/blob/master/cpp/pybind/geometry/kdtreeflann.cpp#L176
+    SearchRadiusData search_radius_vector_3d(KDTreeFlann const & tree, Eigen::Vector3d const & query, double radius){
+        std::vector<int> indices;
+        std::vector<double> distance2;
+        int k = tree.SearchRadius(query, radius, indices, distance2);
+
+        if (k < 0)
+            throw std::runtime_error(
+                    "search_radius_vector_3d() error!");
+
+        return SearchRadiusData({k, indices, distance2});
+    }
+
+    double mvp(std::vector<PointCloud> const & pcs, std::vector<Eigen::Matrix4d> const & ts){
+        const int min_knn = 5;
+        const double knn_rad = 1.0;
+
+        auto pc_map = aggregate_map(pcs, ts);
+
+
+        KDTreeFlann map_tree;
+        map_tree.SetGeometry(pc_map);
+        std::vector<Eigen::Vector3d> points = pc_map.points_;
+
+        std::vector<double> metric;
+        for (const auto& point: points){
+            auto tpl = search_radius_vector_3d(map_tree, point, knn_rad);
+            if (tpl.indices.size() > min_knn){
+                Eigen::MatrixXd tmp = points_idx_to_matrix(points, tpl.indices);
+                Eigen::MatrixXd cov_matrix = cov(tmp);
+                auto eigenvalues = cov_matrix.eigenvalues().real();
+                metric.push_back(*std::min_element(eigenvalues.begin(), eigenvalues.end()));
+            }
+        }
+        return (metric.empty() ? 0 : std::reduce(metric.begin(), metric.end()) / static_cast<double>(metric.size()));
+    }
+
+}
