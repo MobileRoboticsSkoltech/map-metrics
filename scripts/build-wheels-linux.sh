9621a6afffd28b80281f8196bb5bef3a430823f7
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2021-12-08 22:40:05 +0300

    Removed parallel jobs

--- scripts/build-wheels-linux.sh
+++ scripts/build-wheels-linux.sh
@@ -49,7 +49,7 @@ do
              -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$PWD/../bin \
              -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$PWD/../map_metrics \
              -DCMAKE_CXX_FLAGS=-std=c++17 \
-    && cmake --build . -j $(nproc)
+    && cmake --build .
 done
 
 cd ../
