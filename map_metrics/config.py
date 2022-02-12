724dc667fa4f16b0e70af597d8e0b83cff7d1340
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-02-12 19:28:50 +0300

    Depth, Lidar, Custom configs

--- map_metrics/config.py
+++ map_metrics/config.py
@@ -1,20 +1,45 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
-class Config(ABC):
-    pass
+__all__ = ['BaseConfig', 'DepthConfig', 'LidarConfig', 'CustomConfig']
 
 
-class DepthConfig(Config):
-    def __init__(self):
-        pass
+class BaseConfig(ABC):
+    """
+    Base Config Class
+    """
+    KNN_RAD = None
+    MIN_KNN = None
 
 
-class LidarConfig(Config):
-    def __init__(self):
-        pass
+class DepthConfig(BaseConfig):
+    """
+    Config recommended for data obtained from Depth Camera
+    """
+    KNN_RAD = 0.2
+    MIN_KNN = 5
 
 
-class CustomConfig(Config):
-    def __init__(self):
-        pass
+class LidarConfig(BaseConfig):
+    """
+    Config recommended for data obtained from LiDAR
+    """
+    KNN_RAD = 1
+    MIN_KNN = 5
+
+
+class CustomConfig(BaseConfig):
+    """
+    Configurable Config
+
+    Attributes
+    ----------
+    KNN_RAD: float, default=1.0
+        Estimated radius between a pair of points on the map
+        The value is given in meters
+    MIN_KNN: int, default=5
+        Minimum number of a point neighbors
+    """
+    def __init__(self,  knn_rad=1.0, min_knn=5):
+        self.KNN_RAD = knn_rad
+        self.MIN_KNN = min_knn
