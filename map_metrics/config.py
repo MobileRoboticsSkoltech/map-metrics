ed0dcc3dfcdda1838a5d3ae5350b40975051d485
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-02-16 17:52:12 +0300

    Added min cluster size to config

--- map_metrics/config.py
+++ map_metrics/config.py
@@ -12,6 +12,7 @@ class BaseConfig(ABC):
     KNN_RAD = None
     MIN_KNN = None
     MAX_NN = None
+    MIN_CLUST_SIZE = None
 
 
 class DepthConfig(BaseConfig):
@@ -22,6 +23,7 @@ class DepthConfig(BaseConfig):
     KNN_RAD = 0.2
     MIN_KNN = 5
     MAX_NN = 30
+    MIN_CLUST_SIZE = 5
 
 
 class LidarConfig(BaseConfig):
@@ -32,6 +34,7 @@ class LidarConfig(BaseConfig):
     KNN_RAD = 1
     MIN_KNN = 5
     MAX_NN = 30
+    MIN_CLUST_SIZE = 5
 
 
 class CustomConfig(BaseConfig):
@@ -47,9 +50,12 @@ class CustomConfig(BaseConfig):
         Minimum number of a point neighbors
     MAX_NN: int, default=30
         At most MAX_NN nearest neighbors that have distances to the anchor point less than a given radius
+    MIN_CLUST_SIZE: int, default=5
+        Minimal acceptable cluster size in orthogonal extraction
     """
 
-    def __init__(self, knn_rad=1.0, min_knn=5, max_nn=30):
+    def __init__(self, knn_rad=1.0, min_knn=5, max_nn=30, min_clust_size=5):
         self.KNN_RAD = knn_rad
         self.MIN_KNN = min_knn
         self.MAX_NN = max_nn
+        self.MIN_CLUST_SIZE = min_clust_size
