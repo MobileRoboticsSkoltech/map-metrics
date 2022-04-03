b1d612a8b7dca3c20c9f555fac029d6882af29bd
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-04-04 01:08:51 +0300

    feat: filterClusters function

--- cpp/tests/test_orthogonal.cpp
+++ cpp/tests/test_orthogonal.cpp
@@ -22,36 +22,51 @@
 //     //std::cout << normals.col(0) << std::endl;
 // }
 
-TEST(Orthogonal, BuildNormalsAndLambdas){
-    std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
-    auto pc = cilantro::PointCloud3d(PCs[2]);  // PC-0091.ply
+// TEST(Orthogonal, BuildNormalsAndLambdas){
+//     std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
+//     auto pc = cilantro::PointCloud3d(PCs[2]);  // PC-0091.ply
 
-    auto cut_pc = orth_extract::EstimateNormals(pc, 0.2, 30);
-    auto normals = cut_pc.normals;
+//     auto cut_pc = orth_extract::EstimateNormals(pc, 0.2, 30);
+//     auto normals = cut_pc.normals;
 
-    // Define clusterizer
-    alglib::clusterizerstate s;
-    alglib::ahcreport rep;
-    alglib::clusterizercreate(s);
+//     // Define clusterizer
+//     alglib::clusterizerstate s;
+//     alglib::ahcreport rep;
+//     alglib::clusterizercreate(s);
 
-    // Fit Data
-    alglib::real_2d_array xy;
-    xy.setcontent(normals.cols(), 3, normals.data());
-    constexpr int disttype = 2;
+//     // Fit Data
+//     alglib::real_2d_array xy;
+//     xy.setcontent(normals.cols(), 3, normals.data());
+//     constexpr int disttype = 2;
 
-    // Labels variable definition
-    alglib::integer_1d_array labels;
-    alglib::integer_1d_array cz;
-    alglib::ae_int_t number_of_clusters;
+//     // Labels variable definition
+//     alglib::integer_1d_array labels;
+//     alglib::integer_1d_array cz;
+//     alglib::ae_int_t number_of_clusters;
 
-    // Clustering
-    alglib::clusterizersetpoints(s, xy, disttype);
-    alglib::clusterizerrunahc(s, rep);
+//     // Clustering
+//     alglib::clusterizersetpoints(s, xy, disttype);
+//     alglib::clusterizerrunahc(s, rep);
     
-    // top clusters from  hierarchical  clusterization  tree which are separated by distance >R
-    // Each  of  the clusters stand on its own, no heirarchy
-    alglib::clusterizerseparatedbydist(rep, 1e-1, number_of_clusters, labels, cz);
+//     // top clusters from  hierarchical  clusterization  tree which are separated by distance >R
+//     // Each  of  the clusters stand on its own, no heirarchy
+//     alglib::clusterizerseparatedbydist(rep, 1e-1, number_of_clusters, labels, cz);
 
-    //std::cout << labels.tostring().c_str() << std::endl;
+//     //std::cout << labels.tostring().c_str() << std::endl;
     
+// }
+
+TEST(Random, RandTest){
+    cilantro::VectorSet3d a(3, 3);
+    a << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+
+    std::cout << "Vector a: \n" << a << std::endl;
+
+    Eigen::VectorXd bb(5);
+    bb << 1, 2, 3, 1, 5;
+
+    std::cout << "COLS: " << (bb.array() == 1).cols() << "ROWS: " << (bb.array() == 1).rows() << std::endl; 
+
+    // Put normalized mean of a normal vectors
+    std::cout << a.rowwise().mean().normalized() << std::endl;
 }
