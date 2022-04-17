43a4f1f4722b1646df7deddff0a6641db2bc7872
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-04-17 14:01:50 +0300

    feat: uncommented depth tests

--- cpp/tests/test_depth.cpp
+++ cpp/tests/test_depth.cpp
@@ -1,61 +1,61 @@
-// #include <gtest/gtest.h>
-// #include <chrono>
-// #include <iostream>
+#include <gtest/gtest.h>
+#include <chrono>
+#include <iostream>
 
-// #include "cilantro/3rd_party/tinyply/tinyply.h"
+#include "cilantro/3rd_party/tinyply/tinyply.h"
 
-// #include "parse_utils.h"
-// #include "pc_utils.h"
+#include "parse_utils.h"
+#include "pc_utils.h"
 
-// #include "metrics.h"
+#include "metrics.h"
 
-// namespace kdsearch{
-//     double GetDistance(Eigen::Vector3d const & x, Eigen::Vector3d const & y){
-//         double distance = sqrt(pow(x[0] - y[0], 2.0) + pow(x[1] - y[1], 2.0) + pow(x[2] - y[2], 2.0));
-//         return distance;
-//     }
+namespace kdsearch{
+    double GetDistance(Eigen::Vector3d const & x, Eigen::Vector3d const & y){
+        double distance = sqrt(pow(x[0] - y[0], 2.0) + pow(x[1] - y[1], 2.0) + pow(x[2] - y[2], 2.0));
+        return distance;
+    }
 
-//     std::vector<Eigen::Index> radius_search(cilantro::VectorSet3d const & points, 
-//                                             Eigen::Vector3d const & query, 
-//                                             double radius){
-//         std::vector<Eigen::Index> indices;
-//         for (Eigen::Index i = 0; i < points.cols(); ++i){
-//             if (GetDistance(query, points.col(i)) < radius)
-//                 indices.push_back(i);
-//         }
+    std::vector<Eigen::Index> radius_search(cilantro::VectorSet3d const & points, 
+                                            Eigen::Vector3d const & query, 
+                                            double radius){
+        std::vector<Eigen::Index> indices;
+        for (Eigen::Index i = 0; i < points.cols(); ++i){
+            if (GetDistance(query, points.col(i)) < radius)
+                indices.push_back(i);
+        }
 
-//         return indices;
-//     }
-// }
+        return indices;
+    }
+}
 
-// TEST(DepthTest, MME){
-//     std::vector<cilantro::VectorSet3d> pcs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
-//     std::vector<Eigen::Matrix4d> poses = parse_utils::depth_parse::GetDepthPoses("data/depth/poses");
-//     ASSERT_EQ(pcs.size(), 28);    ASSERT_EQ(pcs.size(), poses.size());
+TEST(DepthTest, MME){
+    std::vector<cilantro::VectorSet3d> pcs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
+    std::vector<Eigen::Matrix4d> poses = parse_utils::depth_parse::GetDepthPoses("data/depth/poses");
+    ASSERT_EQ(pcs.size(), 28);    ASSERT_EQ(pcs.size(), poses.size());
 
-//     auto start_time = std::chrono::system_clock::now();
-//     double actual_mme = metrics::GetMME(pcs, poses, config::CustomConfig(5, 0.2, 30, 5));
-//     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-//             std::chrono::system_clock::now() - start_time);
+    auto start_time = std::chrono::system_clock::now();
+    double actual_mme = metrics::GetMME(pcs, poses, config::CustomConfig(5, 0.2, 30, 5));
+    auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now() - start_time);
 
-//     std::cout << "MME: " << actual_mme << std::endl;
-//     std::cout << "Elapsed time (ms): " << elapsed_milliseconds.count() << std::endl;
+    std::cout << "MME: " << actual_mme << std::endl;
+    std::cout << "Elapsed time (ms): " << elapsed_milliseconds.count() << std::endl;
 
-//     ASSERT_LE(std::fabs(actual_mme - -3.6144387057257523), 1e-8);
-// }
+    ASSERT_LE(std::fabs(actual_mme - -3.6144387057257523), 1e-8);
+}
 
-// TEST(DepthTest, MPV){
-//     std::vector<cilantro::VectorSet3d> pcs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
-//     std::vector<Eigen::Matrix4d> poses = parse_utils::depth_parse::GetDepthPoses("data/depth/poses");
-//     ASSERT_EQ(pcs.size(), 28);    ASSERT_EQ(pcs.size(), poses.size());
+TEST(DepthTest, MPV){
+    std::vector<cilantro::VectorSet3d> pcs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
+    std::vector<Eigen::Matrix4d> poses = parse_utils::depth_parse::GetDepthPoses("data/depth/poses");
+    ASSERT_EQ(pcs.size(), 28);    ASSERT_EQ(pcs.size(), poses.size());
 
-//     auto start_time = std::chrono::system_clock::now();
-//     double actual_mpv = metrics::GetMPV(pcs, poses, config::CustomConfig(5, 0.2, 30, 5));
-//     auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-//             std::chrono::system_clock::now() - start_time);
+    auto start_time = std::chrono::system_clock::now();
+    double actual_mpv = metrics::GetMPV(pcs, poses, config::CustomConfig(5, 0.2, 30, 5));
+    auto elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now() - start_time);
 
-//     std::cout << "MPV: " << actual_mpv << std::endl;
-//     std::cout << "Elapsed time (ms): " << elapsed_milliseconds.count() << std::endl;
+    std::cout << "MPV: " << actual_mpv << std::endl;
+    std::cout << "Elapsed time (ms): " << elapsed_milliseconds.count() << std::endl;
 
-//     ASSERT_LE(std::fabs(actual_mpv - 0.0025390347963358565), 1e-8);
-// }
+    ASSERT_LE(std::fabs(actual_mpv - 0.0025390347963358565), 1e-8);
+}
--- cpp/tests/test_orthogonal.cpp
+++ cpp/tests/test_orthogonal.cpp
@@ -10,70 +10,24 @@
 #include <iostream>
 #include <dataanalysis.h>
 
+#ifdef TEST_ORTHOGONAL_METRICS
 
-// TEST(Orthogonal, NormalEstimation){
-//     std::vector<cilantro::VectorSet3d> PCs = GetPointClouds("data/kitti_00");
-//     auto pc = cilantro::PointCloud3d(PCs[0]);
+TEST(Orthogonal, FilterClusters){
+    std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
+    auto pc = cilantro::PointCloud3d(PCs[2]);  // PC-0091.ply
 
-//     pc = orth_extract::EstimateNormals(pc, 1.0, 30);
-    
-//     ASSERT_TRUE(pc.hasNormals());
+    // Note: cut_pc gives same result as Python version
+    auto cut_pc = orth_extract::EstimateNormals(pc, 0.2, 30);
+    auto normals = cut_pc.normals;
 
-//     auto normals = pc.normals;
-//     std::vector<double> n(normals.col(0).begin(), normals.col(0).end());
-//     //std::cout << normals.col(0) << std::endl;
-// }
+    clustering::ClusterMeans cluster_means = clustering::ClusterizeAHC(normals, 1e-1);
+    cluster_means.filterClusters(normals, 5);
 
-// TEST(Orthogonal, BuildNormalsAndLambdas){
-//     std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
-//     auto pc = cilantro::PointCloud3d(PCs[2]);  // PC-0091.ply
+    auto cluster_means_pc = cilantro::PointCloud3d();
+    cluster_means_pc.points = cluster_means.getMeans();
 
-//     auto cut_pc = orth_extract::EstimateNormals(pc, 0.2, 30);
-//     auto normals = cut_pc.normals;
-
-//     // Define clusterizer
-//     alglib::clusterizerstate s;
-//     alglib::ahcreport rep;
-//     alglib::clusterizercreate(s);
-
-//     // Fit Data
-//     alglib::real_2d_array xy;
-//     xy.setcontent(normals.cols(), 3, normals.data());
-//     constexpr int disttype = 2;
-
-//     // Labels variable definition
-//     alglib::integer_1d_array labels;
-//     alglib::integer_1d_array cz;
-//     alglib::ae_int_t number_of_clusters;
-
-//     // Clustering
-//     alglib::clusterizersetpoints(s, xy, disttype);
-//     alglib::clusterizerrunahc(s, rep);
-    
-//     // top clusters from  hierarchical  clusterization  tree which are separated by distance >R
-//     // Each  of  the clusters stand on its own, no heirarchy
-//     alglib::clusterizerseparatedbydist(rep, 1e-1, number_of_clusters, labels, cz);
-
-//     //std::cout << labels.tostring().c_str() << std::endl;
-    
-// }
-
-// TEST(Orthogonal, FilterClusters){
-//     std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
-//     auto pc = cilantro::PointCloud3d(PCs[2]);  // PC-0091.ply
-
-//     // Note: cut_pc gives same result as Python version
-//     auto cut_pc = orth_extract::EstimateNormals(pc, 0.2, 30);
-//     auto normals = cut_pc.normals;
-
-//     clustering::ClusterMeans cluster_means = clustering::ClusterizeAHC(normals, 1e-1);
-//     cluster_means.filterClusters(normals, 5);
-
-//     auto cluster_means_pc = cilantro::PointCloud3d();
-//     cluster_means_pc.points = cluster_means.getMeans();
-
-//     cluster_means_pc.toPLYFile("cluster_means_pc_cpp_2.ply");
-// }
+    cluster_means_pc.toPLYFile("cluster_means_pc_cpp_2.ply");
+}
 
 TEST(Orthogonal, CliqueEstimation){
     std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
@@ -88,3 +42,57 @@ TEST(Orthogonal, CliqueEstimation){
 
     max_clique::FindMaxClique(cluster_means, 1e-1);
 }
+
+TEST(Orthogonal, OrthSubsetEstimation){
+   std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
+   auto pc = cilantro::PointCloud3d(PCs[2]);  // PC-0091.ply
+
+   auto orth_subset = orth_extract::ExtractOrthogonalSubset(pc, config::CustomConfig(5, 0.2, 30, 5));
+
+   cilantro::PointCloud3d orth_pc;
+   for (auto const & points : orth_subset){
+       orth_pc.append(cilantro::PointCloud3d(points));
+   }
+
+   orth_pc.toPLYFile("orth_PC_cpp.ply");
+}
+
+TEST(Orthogonal, MOM){
+    std::vector<cilantro::VectorSet3d> PCs = parse_utils::depth_parse::GetDepthPCs("data/depth/pcs");
+    std::vector<Eigen::Matrix4d> poses = parse_utils::depth_parse::GetDepthPoses("data/depth/poses");
+    std::vector<Eigen::Matrix4d> auto_poses = parse_utils::depth_parse::GetDepthPoses("data/depth/auto-poses");
+    auto pc = cilantro::PointCloud3d(PCs[2]);  // PC-0091.ply
+//    auto pivot_pose = poses[2];
+//    cilantro::RigidTransform3d transform_mx(pivot_pose);
+
+    auto orth_subset = orth_extract::ExtractOrthogonalSubset(pc, config::CustomConfig(5, 0.2, 30, 5));
+
+//    // orth_subset rigid transformation
+//    for (size_t i = 0; i < orth_subset.size(); ++i){
+//        auto pc_transformed = cilantro::PointCloud3d(orth_subset[i]).transformed(transform_mx);
+//        orth_subset[i] = pc_transformed.points;
+//    }
+
+    std::cout << "MOM-init: " <<
+        metrics::ComputeOrthogonalMetrics(
+                PCs,
+                poses,
+                config::CustomConfig(5, 0.2, 30, 5),
+                &metrics_utils::metrics_algorithm::ComputeEigenvalues,
+                orth_subset
+                )
+    << std::endl;
+
+        std::cout << "MOM-auto: " <<
+        metrics::ComputeOrthogonalMetrics(
+                PCs,
+                auto_poses,
+                config::CustomConfig(5, 0.2, 30, 5),
+                &metrics_utils::metrics_algorithm::ComputeEigenvalues,
+                orth_subset
+                )
+    << std::endl;
+
+}
+
+#endif // TEST_ORTHOGONAL_METRICS
